8791ea54c2f15818c6adac16820f882fed72a149
Amir Kumalov <kumalovam@mircpay.ru>
2024-12-02 11:43:51 +0300

    Enables the styled-components SWC transform

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
